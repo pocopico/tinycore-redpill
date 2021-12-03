3655e4f553c8efe8ae3a79acdc22e61f87ea1de5
pocopico <pasafiogos@gmail.com>
2021-12-03 18:06:05 +0200

    Added functio to create json from installed modules

--- rploader.sh
+++ rploader.sh
@@ -563,6 +563,34 @@ rm lspci
 
 }
 
+getmodulealiasjson(){
+
+
+echo "{"
+echo "\"modules\" : ["
+
+for module in `ls *.ko`
+do
+echo "{"
+        if [ `modinfo ./$module --field alias |grep -ie pci -ie usb | wc -l` -ge 1 ] ; then
+        echo "\"name\" : \"${module}\","
+        modinfo ./$module --field alias |grep -ie pci -ie usb | awk '{ print "\"alias\" : \"" $1 "\" ,"}' | sed '$ s/,//'
+        else
+        echo "\"name\" : \"${module}\""
+        fi
+
+echo "},"
+done | sed '$ s/,//'
+
+echo "]"
+echo "}"
+
+
+
+
+
+}
+
 if [ $# -lt 2 ] ; then
 showhelp
 exit 99
